5d4d0ffd4df7c987ac90e4451694dbb15a9dd865
Viet Hoang <hoangviet18306@gmail.com>
2021-01-22 22:29:14 +0700

    Ultilies file

---
+++ StudyPlan/StudyPlan/Ultilities/utilities.cpp
@@ -0,0 +1,16 @@
+//
+//  .cpp
+//  StudyPlan
+//
+//  Created by Hoang Viet on 05/12/2020.
+//
+
+#include <iostream>
+#include<curses.h>
+
+using namespace std;
+
+int utilities() {
+    // insert code here...
+    return 0;
+}
