6f93aea32763615382978979234645701e44dc65
Viet Hoang <hoangviet18306@gmail.com>
2021-01-22 22:21:10 +0700

    main file

--- StudyPlan/StudyPlan/main.cpp
+++ StudyPlan/StudyPlan/main.cpp
@@ -1,9 +1,14 @@
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
+#include <cmath>
 #include <string>
-using namespace std;
+#include "PointOfSale.hpp"
 
-int main() {
-    
+using namespace std;
+using namespace POS;
+int maintain() {
+    POS::myclass mc;
+    mc.pointofsale();
     return 0;
 }
-
